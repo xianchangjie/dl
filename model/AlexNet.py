c0263682fa72d77cf91a0b9ccb19acd9a1ef125b
xianchangjie <xianchangjie@gmail.com>
2021-05-14 17:14:47 +0800

    LeNet added

--- model/AlexNet.py
+++ model/AlexNet.py
@@ -1,1 +1,7 @@
 import tensorflow as tf
+
+def net():
+    sequential = tf.keras.models.Sequential()
+    sequential.add(tf.keras.layers.Conv2D(filters=96,kernel_size=11,strides=4,activation='relu'))
+    sequential.add(tf.keras.layers.MaxPool2D(pool_size=3,strides=2))
+    sequential.add(tf.keras.layers.Conv2D(filters=256,kernel_size=5,padding='same',activation='relu'))
